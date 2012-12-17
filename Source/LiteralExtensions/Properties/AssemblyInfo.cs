6c22dda118000ae58cda57aa672461ae0cb2af8c
Daniel Hilgarth <d.hilgarth@fire-development.com>
2012-12-17 01:14:54 +0100

    Cleaned and updated AssemblyInfo files and introduced SharedAssemblyInfo.cs

--- Source/LiteralExtensions/Properties/AssemblyInfo.cs
+++ Source/LiteralExtensions/Properties/AssemblyInfo.cs
@@ -6,32 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LiteralExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides literal extensions methods that allow to write stuff like 5.Days() + 42.Minutes()")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LiteralExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("303650f6-a4e2-4573-85d2-5d6ce293623e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
---
+++ Source/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Daniel Hilgarth")]
+[assembly: AssemblyCopyright("Copyright © Daniel Hilgarth 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("LiteralExtensions")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
